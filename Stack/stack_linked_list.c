e9f501970181ecd6ad927645bfacc4714a0f5ba8
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2024-04-21 00:31:23 +0530

    Stack error is solved.

--- Stack/stack_linked_list.c
+++ Stack/stack_linked_list.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct list_node {
+struct list_node
+{
     int val;
     struct list_node *next;
 };
 
-struct stack {
+struct stack
+{
     struct list_node *top;
     struct list_node *head;
 };
-
-void create_stack(struct stack *s);
-
-struct list_node *create_node();
 
 void print_stack(struct stack *s);
 
@@ -28,111 +26,100 @@ bool isFull(struct stack *s);
 
 void peek(struct stack *s);
 
-int main() {
+int main()
+{
     int choice;
     bool controller = true;
-    struct stack *s = (struct stack *) malloc(sizeof(struct stack));
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
     s->top = NULL;
-    while (controller) {
-        printf("Enter 1 for creating a stack. \n");
-        printf("Enter 2 for printing a stack. \n");
-        printf("Enter 3 for pushing an element in the stack. \n");
-        printf("Enter 4 for popping an element from the stack. \n");
-        printf("Enter 5 for checking the stack is empty or not. \n");
-        printf("Enter 6 for checking the stack is full or not. \n");
-        printf("Enter 7 for peek at a position of the array. \n");
-        printf("Enter your choice:");
+    s->head = NULL;
+    while (controller)
+    {
+        printf("Enter 1 for printing a stack. \n");
+        printf("Enter 2 for pushing an element in the stack. \n");
+        printf("Enter 3 for popping an element from the stack. \n");
+        printf("Enter 4 for checking the stack is empty or not. \n");
+        printf("Enter 5 for checking the stack is full or not. \n");
+        printf("Enter 6 for peek at a position of the array. \n");
+        printf("Enter your choice: ");
         scanf("%d", &choice);
-        if (s->top != NULL){
-            printf("%d\n", s->top->val);
-        }
-        switch (choice) {
-            case 1:
-                create_stack(s);
-                break;
-            case 2:
-                print_stack(s);
-                break;
-            case 3:
-                push(s);
-                break;
-            case 4:
-                pop(s);
-                break;
-            case 5:
-                isEmpty(s) ? printf("The stack is empty. \n") : printf("The stack is not empty \n");
-                break;
-            case 6:
-                isFull(s) ? printf("The stack is full. \n") : printf("The stack is not full. \n");
-                break;
-            case 7:
-                peek(s);
-                break;
-            default:
-                printf("The program is terminated. \n");
-                controller = false;
-                break;
+        switch (choice)
+        {
+        case 1:
+            print_stack(s);
+            break;
+        case 2:
+            push(s);
+            break;
+        case 3:
+            pop(s);
+            break;
+        case 4:
+            isEmpty(s) ? printf("The stack is empty. \n") : printf("The stack is not empty \n");
+            break;
+        case 5:
+            isFull(s) ? printf("The stack is full. \n") : printf("The stack is not full. \n");
+            break;
+        case 6:
+            peek(s);
+            break;
+        default:
+            printf("The program is terminated. \n");
+            controller = false;
+            break;
         }
     }
     free(s);
     return 0;
 }
 
-struct list_node *create_node() {
-    int val;
-    struct list_node *node = (struct list_node *) malloc(sizeof(struct list_node));
-    if (node == NULL) {
-        free(node);
-        return NULL;
-    }
-    printf("Enter a value:");
-    scanf("%d", &val);
-    if (val < 0) {
-        free(node);
-        return NULL;
-    }
-    node->val = val;
-    node->next = create_node();
-    return node;
-}
-
-void create_stack(struct stack *s) {
-    s->top = NULL;
-    s->head = create_node();
-    s->top = s->head;
-}
-
-void print_stack(struct stack *s) {
+void print_stack(struct stack *s)
+{
     struct list_node *current = s->head;
-    while (current != NULL) {
-        printf("%d \t", current->val);
-        current = current->next;
+    if (s->head == NULL)
+    {
+        printf("The stack is empty. \n");
     }
-    printf("\n");
+    else
+    {
+        while (current != NULL)
+        {
+            printf("%d \t", current->val);
+            current = current->next;
+        }
+        printf("\n");
+    }
 }
 
-void push(struct stack *s) {
+void push(struct stack *s)
+{
     int val;
-    struct list_node *node = (struct list_node *) malloc(sizeof(struct list_node));
-    printf("Enter a value:");
+    struct list_node *node = (struct list_node *)malloc(sizeof(struct list_node));
+    printf("Enter a value: ");
     scanf("%d", &val);
-    if (val < 0) {
+    if (val < 0)
+    {
         free(node);
         printf("You can not add negative numbers. \n");
-    } else {
-        node->val=val;
+    }
+    else
+    {
+        node->val = val;
         node->next = s->head;
         s->head = node;
         s->top = node;
     }
-
 }
 
-void pop(struct stack *s) {
+void pop(struct stack *s)
+{
     struct list_node *node;
-    if (isEmpty(s)) {
+    if (isEmpty(s))
+    {
         printf("There is no element to pop. \n");
-    } else {
+    }
+    else
+    {
         node = s->head;
         s->top = s->head->next;
         s->head = s->head->next;
@@ -140,16 +127,20 @@ void pop(struct stack *s) {
     }
 }
 
-bool isEmpty(struct stack *s) {
-    if (s->top == NULL) {
+bool isEmpty(struct stack *s)
+{
+    if (s->top == NULL)
+    {
         return true;
     }
     return false;
 }
 
-bool isFull(struct stack *s) {
-    struct list_node *node = (struct list_node *) malloc(sizeof(struct list_node));
-    if (node == NULL) {
+bool isFull(struct stack *s)
+{
+    struct list_node *node = (struct list_node *)malloc(sizeof(struct list_node));
+    if (node == NULL)
+    {
         free(node);
         return true;
     }
@@ -157,20 +148,14 @@ bool isFull(struct stack *s) {
     return false;
 }
 
-void peek(struct stack *s) {
-    int position, i=0;
-    struct list_node *current = s->head;
-//    position starts with 0
-    printf("Enter the position of the value, you want to access:");
-    scanf("%d", &position);
-    while(i<(position-1) && current!=NULL){
-        current = current->next;
-        i++;
+void peek(struct stack *s)
+{
+    if (s->head != NULL)
+    {
+        printf("%d \n", s->head->val);
     }
-    if (current!=NULL){
-    printf("%d \n", current->val);
-    }
-    else{
+    else
+    {
         printf("There is not any value at this index.");
     }
 }
