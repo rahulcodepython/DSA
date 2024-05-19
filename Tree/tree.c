9967b1b7e50cba5040e150466bdd81ea5efbfc0a
Rahul Das <77882624+rahulcodepython@users.noreply.github.com>
2024-05-19 07:36:47 +0530

    Binary Search Tree is done.

--- Tree/tree.c
+++ Tree/tree.c
@@ -2,30 +2,92 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct node
+struct tree
 {
     int data;
-    struct node *left;
-    struct node *right;
+    struct tree *left;
+    struct tree *right;
 };
 
-struct node *create_tree()
+struct tree *add_node(struct tree *root, int value)
 {
-    struct node *root = (struct node *)malloc(sizeof(struct node));
-    root->data = 1;
-    root->left = (struct node *)malloc(sizeof(struct node));
-    root->left->data = 2;
-    root->left->left = NULL;
-    root->left->right = NULL;
-    root->right = (struct node *)malloc(sizeof(struct node));
-    root->right->data = 3;
-    root->right->left = NULL;
-    root->right->right = NULL;
+    struct tree *new_tree_node = (struct tree *)malloc(sizeof(struct tree));
+    if (root == NULL)
+    {
+        new_tree_node->data = value;
+        new_tree_node->left = NULL;
+        new_tree_node->right = NULL;
+        return new_tree_node;
+    }
+    else
+    {
+        if (value < root->data)
+        {
+            root->left = add_node(root->left, value);
+        }
+        else
+        {
+            root->right = add_node(root->right, value);
+        }
+    }
     return root;
+}
+
+struct tree *create_tree(struct tree *root)
+{
+    int data;
+
+    while (true)
+    {
+        printf("Enter the data of the node: ");
+        scanf("%d", &data);
+        if (data < 0)
+        {
+            break;
+        }
+        root = add_node(root, data);
+    }
+    return root;
+}
+
+void print_tree_preorder(struct tree *t)
+{
+    if (t != NULL)
+    {
+        printf("%d  ", t->data);
+        print_tree_preorder(t->left);
+        print_tree_preorder(t->right);
+    }
+}
+
+void print_tree_inorder(struct tree *t)
+{
+    if (t != NULL)
+    {
+        print_tree_preorder(t->left);
+        printf("%d  ", t->data);
+        print_tree_preorder(t->right);
+    }
+}
+
+void print_tree_postorder(struct tree *t)
+{
+    if (t != NULL)
+    {
+        print_tree_preorder(t->left);
+        print_tree_preorder(t->right);
+        printf("%d  ", t->data);
+    }
 }
 
 int main()
 {
-
+    struct tree *root = NULL;
+    root = create_tree(root);
+    print_tree_preorder(root);
+    printf("\n");
+    print_tree_inorder(root);
+    printf("\n");
+    print_tree_postorder(root);
     return 0;
 }
